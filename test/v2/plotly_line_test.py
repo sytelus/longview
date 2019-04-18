f5cb66f8f62976a3cbb39470829091acbf771ca8
Shital Shah <shital@ShitalShah.com>
2019-04-17 22:40:01 -0700

    added plotly test

--- test/v2/plotly_line_test.py
+++ test/v2/plotly_line_test.py
@@ -1,5 +1,5 @@
 from tensorwatch.v2.watcher import Watcher
-from tensorwatch.v2.mpl.line_plot import LinePlot
+from tensorwatch.v2.plotly.line_plot import LinePlot
 from tensorwatch.v2.image_utils import plt_loop
 from tensorwatch.v2.publisher import Publisher
 from tensorwatch.v2.lv_types import StreamItem
@@ -14,10 +14,6 @@ def main():
 
     for i in range(5):
         watcher.observe(x=(i, i*i))
-    plt_loop()
 
 main()
-
-
-
 
